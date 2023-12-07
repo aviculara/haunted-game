2a8a5be0424bbcfdd285f9da6601bea00fe787fa
Lara <114432707+aviculara@users.noreply.github.com>
2023-12-08 00:44:22 +0300

    enemy movement added, some animations fixed

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,19 +7,26 @@ public class EnemyBehaviour : MonoBehaviour
 {
     public int difficultyLevel = 1;
     public int currentDir = 0;
+    public GameObject playerObject;
+
     private Move moveScript;
     private int nextDir = 0;
+    private Rigidbody2D playerRB;
+    private Vector2 prevDist;
+    private Vector2 currentDist;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Enemy script start");
+        
         moveScript = GetComponent<Move>();
         if(moveScript == null)
         {
             Debug.Log("null");
         }
 
+        playerRB = playerObject.GetComponent<Rigidbody2D>();
+        prevDist = findDistance(playerRB);
     }
 
     void FixedUpdate()
@@ -28,13 +35,30 @@ public class EnemyBehaviour : MonoBehaviour
         {
             case 1:
                 //difficulty level 1
-                nextDir = moveScript.RandomNearbyDirection(currentDir);
-                moveScript.ChangeDirTo(nextDir);
-                currentDir = nextDir;
+                //if distance increased
+                currentDist = findDistance(playerRB);
+                if(currentDist.magnitude > prevDist.magnitude)
+                {
+                    //change direction
+                    nextDir = moveScript.RandomNearbyDirection(currentDir);
+                    moveScript.ChangeDirTo(nextDir);
+                    currentDir = nextDir;
+                }
+                prevDist = currentDist;
                 break;
             default:
                 //idk yet
                 break;
         }
     }
+
+    private Vector2 findDistance(Rigidbody2D targetRB)
+    {
+        Vector2 distance;
+        Vector2 targetPos = targetRB.transform.position;
+        Vector2 selfPos = moveScript.objectToMove.transform.position;
+        distance = targetPos - selfPos;
+        return distance;
+    }
+
 }
