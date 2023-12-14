b01ad744b2934f1094f9c79950839c51e386db45
Lara <114432707+aviculara@users.noreply.github.com>
2023-12-15 00:04:26 +0300

    temporary game complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int score = 0;
     public Text scoreText;
+    public GameObject gameOverPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,17 @@ public class GameManager : MonoBehaviour
     {
         score = score + addedPoints;
         scoreText.text = score.ToString();
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverPanel.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //gameOverPanel.SetActive(false);
+        Time.timeScale = 1;
     }
 }
